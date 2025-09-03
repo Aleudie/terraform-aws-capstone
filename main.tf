3fd1d291965826e4a25d99aa725a1a982613fdd5
Aleudie <dmichel@griddynamics.com>
2025-09-02 20:04:57 -0600

    Allow http

--- main.tf
+++ main.tf
@@ -2,9 +2,31 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = null # optional, defaults to default VPC if not specified
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "web" {
-  ami           = "ami-08c40ec9ead489470" # Amazon Linux 2 AMI (update if needed per region)
+  ami           = "ami-08c40ec9ead489470" # Amazon Linux 2 AMI (us-east-1)
   instance_type = "t2.micro"
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   user_data = <<-EOF
               #!/bin/bash
